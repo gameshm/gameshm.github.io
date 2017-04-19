28519c5bde44f9daaa4c1c0b2c66151db924e949
henryituarte <henryituarte@gmail.com>
2017-04-19 11:03:29 +0200

    SuperMario first commit
    Background loaded but pj keeps falling down ;(

---
+++ SuperMarioBros/practica3/practica3.js
@@ -0,0 +1,108 @@
+var game = function() {
+
+// Set up an instance of the Quintus engine  and include
+// the Sprites, Scenes, Input and 2D module. The 2D module
+// includes the `TileLayer` class as well as the `2d` componet.
+var Q = window.Q = Quintus()
+        .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI, TMX")
+        // Maximize this game to whatever the size of the browser is
+        .setup({ maximize: true })
+        // And turn on default input controls and touch input (for UI)
+        //.controls().touch()
+
+
+// ## Player Sprite
+// The very basic player sprite, this is just a normal sprite
+// using the player sprite sheet with default controls added to it.
+Q.Sprite.extend("Mario",{
+
+  // the init constructor is called on creation
+  init: function(p) {
+
+    // You can call the parent's constructor with this._super(..)
+    this._super(p, {
+      sprite: "mario_small",
+      sheet: "mario_small",  // Setting a sprite sheet sets sprite width and height
+      x: 150,           // You can also set additional properties that can
+      y: 380,           // be overridden on object creation
+      gravity: 1             
+    });
+
+    // Add in pre-made components to get up and running quickly
+    // The `2d` component adds in default 2d collision detection
+    // and kinetics (velocity, gravity)
+    // The `platformerControls` makes the player controllable by the
+    // default input actions (left, right to move,  up or action to jump)
+    // It also checks to make sure the player is on a horizontal surface before
+    // letting them jump.
+    this.add('2d, platformerControls, animation');
+
+    // Write event handlers to respond hook into behaviors.
+    // hit.sprite is called everytime the player collides with a sprite
+    this.on("hit.sprite",function(collision) {
+
+      // Check the collision, if it's the Tower, you win!
+      if(collision.obj.isA("Tower")) {
+        Q.stageScene("endGame",1, { label: "You Won!" }); 
+        this.destroy();
+      }
+    });
+
+  },
+
+  step: function(dt){
+    if(this.p.y > 550){
+      this.p.x = 150;
+      this.p.y = 380;
+      this.p.vy = 0;
+    }
+  }
+
+});
+
+
+Q.load(["mario_small.png", "mario_small.json"], function(){
+        Q.compileSheets("mario_small.png", "mario_small.json");
+      });
+
+Q.scene("level1",function(stage) {
+      Q.stageTMX("level.tmx",stage);
+
+      var mario = stage.insert(new Q.Mario());
+      stage.add("viewport").follow(mario);
+});
+ 
+Q.loadTMX("level.tmx", function() {
+
+    Q.stageScene("level1");
+
+});
+/*
+// ## Level1 scene
+// Create a new scene called level 1
+Q.scene("level1",function(stage) {
+
+  // Add in a repeater for a little parallax action
+  stage.insert(new Q.Repeater({ asset: "background-wall.png", speedX: 0.5, speedY: 0.5 }));
+
+  // Add in a tile layer, and make it the collision layer
+  stage.collisionLayer(new Q.TileLayer({
+                             dataAsset: 'level.json',
+                             sheet:     'tiles' }));
+
+  // Create the player and add them to the stage
+  var player = stage.insert(new Q.Player());
+
+  // Give the stage a moveable viewport and tell it
+  // to follow the player.
+  stage.add("viewport").follow(player);
+
+  // Add in a couple of enemies
+  stage.insert(new Q.Enemy({ x: 700, y: 0 }));
+  stage.insert(new Q.Enemy({ x: 800, y: 0 }));
+
+  // Finally add in the tower goal
+  stage.insert(new Q.Tower({ x: 180, y: 50 }));
+});
+*/
+}
