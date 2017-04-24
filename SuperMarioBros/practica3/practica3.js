a6c2a4bdfa44dd24a1f720ceaeeb1e8ca3f11cde
mlaina <laina855@gmail.com>
2017-04-25 00:28:26 +0200

    end game

--- SuperMarioBros/practica3/practica3.js
+++ SuperMarioBros/practica3/practica3.js
@@ -79,6 +79,7 @@ Q.Sprite.extend("Mario",{
 
   die: function(){
    this.play("die", 1);
+   Q.stageScene("endGame",1, { label: "You Died" }); 
   },
 
   jump:function(){
@@ -99,13 +100,33 @@ Q.Sprite.extend("Mario",{
     } else {
       this.play("stand_" + this.p.direction);
     }
-
+    
     if(this.p.y> 550){
       this.die();
     }
   }
 
 });
+
+//princess 4600 525
+
+Q.Sprite.extend("Princess", {
+  init:function(){
+    this._super({
+      sprite:"princess",
+      sheet:"princess",
+      x: 4600,
+      y: 525, 
+      gravity:1
+    });
+    this.add('2d');
+    this.on("bump.top, bump.right, bump.bottom, bump.left", function(collision){
+      if(collision.obj.isA("Mario"))
+        Q.stageScene("endGame",1, { label: "You win" }); 
+  });
+}
+});
+
 Q.Sprite.extend("Coin", {
   init:function(){
     this._super({
@@ -154,7 +175,6 @@ Q.Sprite.extend("Goomba",{//seta
     });
     this.on("bump.left,bump.right,bump.bottom",function(collision) {
         if(collision.obj.isA("Mario")) { 
-            Q.stageScene("endGame",1, { label: "You Died" }); 
             collision.obj.die();
         }
     });
@@ -202,7 +222,6 @@ Q.Sprite.extend("Bloopa",{//calamar
     });
     this.on("bump.left,bump.right,bump.bottom",function(collision) {
         if(collision.obj.isA("Mario")) { 
-            Q.stageScene("endGame",1, { label: "You Died" }); 
             collision.obj.die();
         }
     });
@@ -228,11 +247,12 @@ Q.load([ "music_main.mp3", "coin.mp3" ], function() {
   Q.audio.play('music_main.mp3',{ loop: true });
 });
 
-Q.load(["coin.png","coin.json","mario_small.png", "mario_small.json", "goomba.png", "goomba.json", "bloopa.png", "bloopa.json"], function(){
+Q.load(["princess.png","princess.json","coin.png","coin.json","mario_small.png", "mario_small.json", "goomba.png", "goomba.json", "bloopa.png", "bloopa.json"], function(){
         Q.compileSheets("mario_small.png", "mario_small.json");
         Q.compileSheets("goomba.png", "goomba.json");
         Q.compileSheets("bloopa.png", "bloopa.json");
         Q.compileSheets("coin.png", "coin.json");
+        Q.compileSheets("princess.png", "princess.json");
       });
 
 Q.scene("level1",function(stage) {
@@ -245,10 +265,10 @@ Q.scene("level1",function(stage) {
         coin[i]= stage.insert(new Q.Coin());
         coin[i].p.x=purse[i].x;
         coin[i].p.y=purse[i].y;
-    }
-
-      //var goomba = stage.insert(new Q.Goomba());
-      //var bloopa = stage.insert(new Q.Bloopa());
+      }
+      var princess=stage.insert(new Q.Princess());
+      var goomba = stage.insert(new Q.Goomba());
+      var bloopa = stage.insert(new Q.Bloopa());
       stage.add("viewport").follow(mario,{ x: true, y: false });
 });
  
@@ -256,6 +276,23 @@ Q.loadTMX("level.tmx", function() {
 
     Q.stageScene("level1");
 
+});
+
+Q.scene('endGame',function(stage) {
+  var container = stage.insert(new Q.UI.Container({
+    x: Q.width/2, y: Q.height/2, fill: "rgba(0,0,0,0.5)"
+  }));
+
+  var button = container.insert(new Q.UI.Button({ x: 0, y: 0, fill: "#CCCCCC",
+                                                  label: "Play Again" }))         
+  var label = container.insert(new Q.UI.Text({x:10, y: -10 - button.p.h, 
+                                                   label: stage.options.label }));
+  button.on("click",function() {
+    Q.clearStages();
+    Q.stageScene('level1');
+  });
+
+  container.fit(20);
 });
 
 
